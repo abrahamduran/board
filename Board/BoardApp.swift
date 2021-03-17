849c1e6d562560daa9f39d06a8ae81e0b3a97ded
Abraham Isaac Durán <youremail@yourdomain.com>
2021-03-16 22:31:09 -0400

    Initial Commit

---
+++ Board/BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardApp.swift
+//  Board
+//
+//  Created by Abraham Isaac Durán on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
